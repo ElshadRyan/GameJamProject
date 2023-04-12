104b09dd272870b6f83c8f2ed9fc27e0137a747d
ElshadRyan <117740569+ElshadRyan@users.noreply.github.com>
2023-04-12 13:58:53 +0700

    Collision Kelar

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,9 +6,8 @@ using System;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
-    [SerializeField] private float speed;
+    [SerializeField] private Transform playerTransform;
     private float moveSpeed = 1;
-    private bool Diagonal = true;
 
 
 
@@ -20,60 +19,66 @@ public class PlayerMove : MonoBehaviour
     }
 
     
-    private void PlayerMovement()
+    public void PlayerMovement()
     {
+        float maxDistance = 2f;
         Vector2 inputVector = new Vector2(0, 0);
-        //Diagonal = true;
 
-
-        if (Input.GetKeyDown(KeyCode.W) && Diagonal)
+        if (Input.GetKeyDown(KeyCode.W))
         {
             inputVector.x = +moveSpeed;
-            Diagonal = false;
             transform.LookAt(transform.position + new Vector3(0, 0, 1));
         }
-        if (Input.GetKeyDown(KeyCode.A) && Diagonal)
-        {
-            inputVector.y = +moveSpeed;
-            Diagonal = false;
-            transform.LookAt(transform.position + new Vector3(-1, 0, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.S) && Diagonal)
-        {
-            inputVector.x = -moveSpeed;     
-            Diagonal = false;
-            transform.LookAt(transform.position + new Vector3(0, 0, -1));
-        }
-        if (Input.GetKeyDown(KeyCode.D) && Diagonal)
+        if (Input.GetKeyDown(KeyCode.A))
         {
             inputVector.y = -moveSpeed;
-            Diagonal = false;
+            transform.LookAt(transform.position + new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            inputVector.x = -moveSpeed;
+            transform.LookAt(transform.position + new Vector3(0, 0, -1));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            inputVector.y = +moveSpeed;
             transform.LookAt(transform.position + new Vector3(1, 0, 0));
         }
-
-
-
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            Diagonal = true;
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            Diagonal = true;
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            Diagonal = true;
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            Diagonal = true;
-        }
-
-        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection;
         
-       
+        Vector3 moveDirection = new Vector3(inputVector.y, 0f, inputVector.x);
+        bool canMove = !Physics.BoxCast(playerTransform.position, transform.localScale, moveDirection, Quaternion.identity, moveSpeed);
+
+
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0f, 0f);
+            canMove = moveDirection.x != 0 && !Physics.BoxCast(playerTransform.position, transform.localScale, moveDirection, Quaternion.identity, moveSpeed);
+
+            if (canMove)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0f, 0f, moveDirection.z);
+                canMove = moveDirection.z != 0 && !Physics.BoxCast(playerTransform.position, transform.localScale, moveDirectionZ, Quaternion.identity, moveSpeed);
+
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ; 
+                }
+            }
+        }
+
+        
+
+        if (canMove)
+        {
+             Debug.Log("bisa Move");
+             transform.position += moveDirection;
+       }
+        Debug.Log(canMove);
+
     }
 
 }
